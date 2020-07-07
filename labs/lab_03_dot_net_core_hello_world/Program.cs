500142a05598d0a9877f329795ae087c91a2666f
LeoRoma <xiajtn@gmail.com>
2020-07-07 13:32:15 +0100

    added projects

---
+++ labs/lab_03_dot_net_core_hello_world/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab_03_dot_net_core_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! from programmer");
+            int[] myArray = {1, 2, 3, 4, 5};
+            foreach(var item in myArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
