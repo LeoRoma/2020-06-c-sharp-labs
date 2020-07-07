1337c3cbf349d4bc5910fadfd0b571cc9970d107
LeoRoma <xiajtn@gmail.com>
2020-07-07 18:02:22 +0100

    Numbers and arithmetic operators are shown at the display

--- labs/lab_07_calculator/MainWindow.xaml.cs
+++ labs/lab_07_calculator/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace lab_07_calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        static string display = "";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -32,82 +34,98 @@ namespace lab_07_calculator
 
         private void Button01_Click(object sender, RoutedEventArgs e)
         {
-
+            display += "1";
+            LabelDisplay.Content = display;
         }
 
         private void Button02_Click(object sender, RoutedEventArgs e)
         {
-
+            display += "2";
+            LabelDisplay.Content = display;
         }
 
         private void Button03_Click(object sender, RoutedEventArgs e)
         {
-
+            display += "3";
+            LabelDisplay.Content = display;
         }
 
         private void Button04_Click(object sender, RoutedEventArgs e)
         {
-
+            display += "4";
+            LabelDisplay.Content = display;
         }
 
         private void Button05_Click(object sender, RoutedEventArgs e)
         {
-
+            display += "5";
+            LabelDisplay.Content = display;
         }
 
         private void Button06_Click(object sender, RoutedEventArgs e)
         {
-
+            display += "6";
+            LabelDisplay.Content = display;
         }
 
         private void Button07_Click(object sender, RoutedEventArgs e)
         {
-
+            display += "7";
+            LabelDisplay.Content = display;
         }
 
         private void Button08_Click(object sender, RoutedEventArgs e)
         {
-
+            display += "8";
+            LabelDisplay.Content = display;
         }
 
         private void Button09_Click(object sender, RoutedEventArgs e)
         {
-
+            display += "9";
+            LabelDisplay.Content = display;
         }
 
         private void Button00_Click(object sender, RoutedEventArgs e)
         {
-
+            display += "0";
+            LabelDisplay.Content = display;
         }
 
         private void ButtonFullStop_Click(object sender, RoutedEventArgs e)
         {
-
+            display += ".";
+            LabelDisplay.Content = display;
         }
 
         private void ButtonDivision_Click(object sender, RoutedEventArgs e)
         {
-
+            display += "/";
+            LabelDisplay.Content = display;
         }
 
         private void ButtonMoltiplication_Click(object sender, RoutedEventArgs e)
         {
-
+            display += "*";
+            LabelDisplay.Content = display;
         }
 
         private void ButtonSubstraction_Click(object sender, RoutedEventArgs e)
         {
-
+            display += "-";
+            LabelDisplay.Content = display;
         }
 
         private void ButtonAddition_Click(object sender, RoutedEventArgs e)
         {
-
+            display += "+";
+            LabelDisplay.Content = display;
         }
 
         private void ButtonEquals_Click(object sender, RoutedEventArgs e)
         {
-
+            //To build at the end 
+            LabelDisplay.Content = display;
         }
     }
 }
