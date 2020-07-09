d2c091f0e2470e5c84822906fc6abd163f7514ce
LeoRoma <xiajtn@gmail.com>
2020-07-09 14:39:47 +0100

    lab_15 tests explanation

--- labs/lab_15_unit_test/Program.cs
+++ labs/lab_15_unit_test/Program.cs
@@ -2,11 +2,26 @@
 
 namespace lab_15_unit_test
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
+    public class Calc
+    {
+        public static int TripleCalc(int a, int b, int c, out int sum)
+        {
+            sum = a + b + c;
+            return a * b * c;
+        }
+
+        public static (int sum, int product) TripleCalc(int a, int b, int c)
+        {
+            return (a + b + c, a * b * c);
+        }
+    }
+
+
 }
