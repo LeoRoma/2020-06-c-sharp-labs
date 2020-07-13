7f2da5147e081acdd5cef8d5b214898156739307
LeoRoma <xiajtn@gmail.com>
2020-07-13 13:14:35 +0100

    lab_20_exceptions_tests added

--- labs/lab_20_exceptions/Program.cs
+++ labs/lab_20_exceptions/Program.cs
@@ -4,9 +4,58 @@ namespace lab_20_exceptions
 {
     class Program
     {
+        private static string[] _theBeatles = new string[] { "John", "Paul", "George", "Ringo" };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //AddBeatle(4, "Brian");
+
+            //int x = 10;
+            //int y = 0;
+
+            //try
+            //{
+            //    int output = x / y;
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine($"An exception occurred {e}");
+            //    Console.WriteLine($"An exception occurred {e.ToString()}");
+            //    Console.WriteLine($"An exception occurred {e.Data}");
+            //    Console.WriteLine($"An exception occurred {e.Message}");
+
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("But life goes on...");
+            //}
+        
+                int three = 3;
+                int sum = int.MaxValue + three;
+                Console.WriteLine(sum);
+
+            
         }
+
+        static void AddBeatle(int pos, string name)
+        {
+            if(pos < 0 || pos >= _theBeatles.Length)
+            {
+                throw new ArgumentException($"The Beatles do have a position {pos}");
+            }
+            _theBeatles[pos] = name;
+            //try
+            //{
+            //    _theBeatles[pos] = name;
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Can't add a fifth Beatle");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Here comes the sun!");
+            //}
+        }
+
     }
 }
---
+++ labs/lab_20_exceptions_tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace lab_20_exceptions_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
