55c1f642d69923cf4c7462e6af2e459a73f0d56c
LeoRoma <xiajtn@gmail.com>
2020-07-13 14:51:20 +0100

    loop_exercise completed

--- labs/lab_19_loop_exercise/Program.cs
+++ labs/lab_19_loop_exercise/Program.cs
@@ -6,26 +6,65 @@ namespace lab_19_loop_exercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Select an option to run a loop");
-            Console.WriteLine("1: prints from 1 to 300");
-            Console.WriteLine("2: for every 100 shows my name");
-            Console.WriteLine("3: each 5 show with 'th'");
-            Console.WriteLine("4: countdown from 50 to 0");
-
-            int userInput = Console.ReadLine();
-            Loop();
+            Console.Write("Select an option to run a loop" +
+                              "\n1: prints from 1 to 300" +
+                              "\n2: for every 100 shows my name" +
+                              "\n3: each 5 show with 'th'" +
+                              "\n4: countdown from 50 to 0\n");
+            int userInput = Int32.Parse(Console.ReadLine());
+            
+            if(userInput > 4)
+            {
+                throw new ArgumentException("Invalid input");
+            }
+            Console.WriteLine($"You selected: {userInput}");
+            UserSelection(userInput);
         }
 
-        static void Loop()
+        static void UserSelection(int userInput)
+        {
+            switch(userInput)
+            {
+                case 1:
+                    Loop1();
+                    break;
+                case 2:
+                    Loop2();
+                    break;
+                case 3:
+                    Loop3();
+                    break;
+                case 4:
+                    Loop4();
+                    break;
+            }
+        }
+
+        static void Loop1()
         {
             for (int i = 1; i <= 300; i++)
             {
-                long lastDigit = i % (10);
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Loop2()
+        {
+            for (int i = 1; i <= 300; i++)
+            {
                 if (i % 100 == 0)
                 {
                     Console.WriteLine("Hi I am Leo");
                 }
-                else if (i == 5 || i == 105 || i == 205)
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Loop3()
+        {
+            for (int i = 1; i <= 300; i++)
+            {
+                if (i == 5 || i == 105 || i == 205)
                 {
                     Console.WriteLine($"{i}th");
                 }
@@ -33,5 +72,12 @@ namespace lab_19_loop_exercise
             }
         }
 
+        static void Loop4()
+        {
+            for (int i = 50; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
