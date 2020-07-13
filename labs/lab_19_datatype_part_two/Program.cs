ea62528913c9b6936890c2f5d09902eb82f366f6
LeoRoma <xiajtn@gmail.com>
2020-07-13 10:02:24 +0100

    lab_19_data_type_part_two created

---
+++ labs/lab_19_datatype_part_two/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab_19_datatype_part_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
