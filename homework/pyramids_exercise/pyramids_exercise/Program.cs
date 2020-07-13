9f64c90c6afc3328975242a4602033de2b69a8d1
LeoRoma <xiajtn@gmail.com>
2020-07-13 23:09:33 +0100

    pyramids_exercise added

---
+++ homework/pyramids_exercise/pyramids_exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pyramids_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
