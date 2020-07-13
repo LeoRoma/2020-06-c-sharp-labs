fc4f222922d541322def0b401f2b7bfe595b4506
LeoRoma <xiajtn@gmail.com>
2020-07-13 16:36:08 +0100

    exercises completed, started highest

--- homework/IterationExercises/IterationExercises/Exercises.cs
+++ homework/IterationExercises/IterationExercises/Exercises.cs
@@ -6,7 +6,7 @@ namespace IterationLib
     public class Program
     {
        
-        public static void Main(int[] nums)
+        public static void Main(string[] nums)
         {
             //ignore
         }
@@ -16,20 +16,71 @@ namespace IterationLib
         // returns the lowest number in the array nums
         public static int Lowest(int[] nums)
         {
-            return 0;
+            
+            if (nums.Length == 0)
+            {
+                return int.MaxValue;
+            }
+            else
+            {
+                int minVal = nums[0];
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    if (nums[i] < minVal)
+                    {
+                        minVal = nums[i];
+                    }
+                }
+                return minVal;
+            }
+
         }
 
         // returns the sum of all numbers between 1 and n inclusive that are divisible by either 2 or 5
         public static int SumEvenFive(int max)
         {
-            return 0;
+            int total = 0;
+            for (int i = 0; i <= max; i++)
+            {
+                if(i % 5 == 0 || i % 2 == 0)
+                {
+                    total += i;
+                }
+            }
+            return total;
         }
 
         // Returns the a string containing the count of As, Bs, Cs and Ds in the parameter string
         // all other letters are ignored
         public static string CountLetters(string input)
         {
-            return String.Empty;
+            input = input.ToUpper();
+            int A = 0;
+            int B = 0;
+            int C = 0;
+            int D = 0;
+            
+            foreach (char letter in input)
+            {
+                if(letter == 'A')
+                {
+                    A += 1;
+                }
+                if (letter == 'B')
+                {
+                    B += 1;
+                }
+                if (letter == 'C')
+                {
+                    C += 1;
+                }
+                if (letter == 'D')
+                {
+                    D += 1;
+                }
+            }
+            string result = $"A:{A} B:{B} C:{C} D:{D}";
+            return result;
         }
     }
 }
