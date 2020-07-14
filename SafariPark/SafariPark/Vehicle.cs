0026bfa16ef796c7ed06089a506d85c264b7190e
LeoRoma <xiajtn@gmail.com>
2020-07-14 14:51:00 +0100

    Vehicle class and test implemented

---
+++ SafariPark/SafariPark/Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafariPark
+{
+    public class Vehicle
+    {
+        private int _capacity;
+        private int _numPassengers;
+        private int _speed;
+
+        public int Position { get; set; }
+        public int NumPassangers { get; set; }
+
+        public Vehicle(int capacity, int speed = 10)
+        {
+            _capacity = capacity;
+            _speed = speed;
+        }
+        public Vehicle()
+        {
+            
+        }
+
+        public int Move(int times)
+        {
+
+            return ;
+        }
+
+        
+    }
+}
