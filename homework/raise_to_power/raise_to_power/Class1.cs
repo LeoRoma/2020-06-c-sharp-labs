a6abe2335d70aa80867582c9420b0da6228c86ee
LeoRoma <xiajtn@gmail.com>
2020-07-14 20:14:35 +0100

    Raise to Power test completed

---
+++ homework/raise_to_power/raise_to_power/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace raise_to_power
+{
+    public class Class1
+    {
+        public static double RaiseToPower(double x, double y, int p)
+        {
+            // 2, 3, 3  ==> (2*3)=6 and raise this to power 3 ie 36*6=216
+            return Math.Pow((x * y), p);
+        }
+    }
+}
