0b12e9f979c98bb3be3c47c6c463db07a534bd3f
LeoRoma <xiajtn@gmail.com>
2020-07-14 14:08:04 +0100

    added struct and nunit

--- SafariPark/SafariPark/Person.cs
+++ SafariPark/SafariPark/Person.cs
@@ -4,7 +4,53 @@ using System.Text;
 
 namespace SafariPark
 {
+    //Class is a reference type of data type, can store more data.
     class Person
     {
+        //Attributes
+        private string _firstName; //Private can be accessed only by this class 
+        private string _lastName;
+        //private int _age;
+
+        //public int Age
+        //{
+        //    get { return _age; }
+        //    set { if (value >= 0) _age = value; }
+        //}
+
+        //Property -> a method that can get and set a value; get = read, set = write
+        public int Age { get; set; }
+
+        //We can use Property as method
+        //public string GetFullName
+        //{
+        //    get { return $"{_firstName} {_lastName}"; }
+        //}
+
+        //Constructor must have the same name as the class --> doesn't return a value
+        //ctor -> shortcut to create a constructor
+        //As for a method we can overload contructors and set a default parameter
+        public Person(string fName, string lName)
+        {
+            _firstName = fName;
+            _lastName = lName;
+        }
+
+        //public Person(string fName, string lName, int age)
+        //{
+        //    _firstName = fName;
+        //    _lastName = lName;
+        //    _age = age;
+        //}
+
+        //public Person()
+        //{
+
+        //}
+        public string GetFullName()
+        {
+            return $"{_firstName} {_lastName}";
+        }
+
     }
 }
---
+++ SafariPark/SafariPark/Point3D.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace SafariPark
+{
+    public struct Point3D
+    {
+        //Struct is a value type data type to compare to class, it helps to store some volatile value, wehre we don't store them permantely
+        public int x, y, z;
+        public Point3D(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        
+    }
+}
