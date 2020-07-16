7749ffb887ce42f6b059c02285e41b5e4b7e039b
LeoRoma <xiajtn@gmail.com>
2020-07-16 10:19:23 +0100

    Airplane tests implemented

--- SafariPark/Airplane_Test/UnitTest1.cs
+++ SafariPark/Airplane_Test/UnitTest1.cs
@@ -4,12 +4,70 @@ using System.Linq.Expressions;
 
 namespace Airplane_Test
 {
-    public class Tests
+    class AirplaneTests
     {
-        [Test]
-        public void Test1()
+
+        public void Setup()
         {
-            Airplane a = new Airplane(200, 100, "JetRuss") { NumPassengers = 150 };
+        }
+
+        [Test]
+
+        public void WhenMoveCalledItReturnsCorrectValueAfterAscend500andMove3Times()
+        {
+            Airplane a = new Airplane(200, 100, "JetsRUs") { NumPassengers = 150 };
+            a.Ascend(500);
+            a.Move(3);
+
+            var actual = a.Move(3);
+
+            var expected = "Moving along 3 times at an altitude of 500 metres.";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+
+        public void ToStringCorrectWhenAscend500andMove3Times()
+        {
+            Airplane a = new Airplane(200, 100, "JetsRUs") { NumPassengers = 150 };
+            a.Ascend(500);
+            a.Move(3);
+
+            var actual = a.ToString();
+
+            var expected = "Thank you for flying JetsRUs: SafariPark.Airplane capacity: 200 passengers: 150 speed: 100 position: 300 altitude: 500.";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+
+        public void WhenMoveCalledItReturnsCorrectValueAfterAscend500Move3TimesDescend200()
+        {
+            Airplane a = new Airplane(200, 100, "JetsRUs") { NumPassengers = 150 };
+            a.Ascend(500);
+            a.Move(3);
+            a.Descend(200);
+
+            var actual = a.Move();
+
+            var expected = "Moving along at an altitude of 300 metres.";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ToStringCorrectWhenAscend500andMove3TimesDescend200ThenCallMove()
+        {
+            Airplane a = new Airplane(200, 100, "JetsRUs") { NumPassengers = 150 };
+            a.Ascend(500);
+            a.Move(3);
+            a.Descend(200);
+            a.Move();
+            a.Move();
+
+            var actual = a.ToString();
+
+            var expected = "Thank you for flying JetsRUs: SafariPark.Airplane capacity: 200 passengers: 150 speed: 100 position: 500 altitude: 300.";
+            Assert.AreEqual(expected, actual);
         }
     }
 }
