3288f2b786c540faed0293b5f51b79cbd00bb7ee
LeoRoma <xiajtn@gmail.com>
2020-07-17 08:08:07 +0100

    GetPrimeFactors method implemented, need to fix a while loop

--- homework/SumOfPrimeFactors/SumOfPrimeFactors/Program.cs
+++ homework/SumOfPrimeFactors/SumOfPrimeFactors/Program.cs
@@ -6,7 +6,29 @@ namespace SumOfPrimeFactors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(PrimeChecker(24));
+            Console.WriteLine(GetPrimeFactors(24));
+        }
+
+        static int GetPrimeFactors(int number)
+        {
+            int total = 0;
+            for (int i = 2; i <= number; i++)
+            {
+                while(PrimeChecker(i) == true)
+                {
+                    if (number % i == 0 && PrimeChecker(i) == true)
+                    {
+                        Console.WriteLine(i);
+                        Console.WriteLine(number);
+                        total += i;
+                        number /= i;
+                        
+                    }
+                }
+                
+
+            }
+            return total;
         }
 
         static bool PrimeChecker(int number)
@@ -16,10 +38,11 @@ namespace SumOfPrimeFactors
             {
                 if(number % i == 0)
                 {
+                    Console.WriteLine("Check prim" + i);
                     isPrime += 1;
                 }
             }
-            if (isPrime == 2) return true;
+            if (isPrime == 1) return true;
             else return false;
         }
     }
