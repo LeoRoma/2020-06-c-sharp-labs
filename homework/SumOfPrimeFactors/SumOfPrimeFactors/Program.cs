8737d6bec1cbb2111efe2b1f0f68b8dcc125a93f
LeoRoma <xiajtn@gmail.com>
2020-07-17 07:29:05 +0100

    PrimeChecker function implemented

---
+++ homework/SumOfPrimeFactors/SumOfPrimeFactors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SumOfPrimeFactors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PrimeChecker(24));
+        }
+
+        static bool PrimeChecker(int number)
+        {
+            int isPrime = 0;
+            for (int i = 2; i <= number; i++)
+            {
+                if(number % i == 0)
+                {
+                    isPrime += 1;
+                }
+            }
+            if (isPrime == 2) return true;
+            else return false;
+        }
+    }
+}
