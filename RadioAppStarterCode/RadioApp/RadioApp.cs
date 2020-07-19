e87cf4e21ee5f1991557ff9073a4dd02fed20342
LeoRoma <xiajtn@gmail.com>
2020-07-19 12:46:58 +0100

    Implemented volume with tests

--- RadioAppStarterCode/RadioApp/RadioApp.cs
+++ RadioAppStarterCode/RadioApp/RadioApp.cs
@@ -6,12 +6,19 @@ namespace RadioApp
     {
         private int _channel = 1;
         private bool _on;
+        private int _volume = 30;
 
         public int Channel 
         {
             get { return _channel; }  
             set { if (value > 0 && value <= 4 && _on == true) _channel = value; }
         }
+
+        //public int Volume
+        //{
+        //    get { return _volume; }
+        //    set { _volume = value; }
+        //}
 
         public Radio()
         {
@@ -37,6 +44,27 @@ namespace RadioApp
         public void TurnOn()
         {
             _on = true;
+        }
+
+        public int IncreaseVolume()
+        {
+
+            if (_volume > 100)
+            {
+                throw new Exception("Cannot increase the volume over 100.");
+            }
+            _volume += 1;
+            return _volume;
+        }
+
+        public int DecreaseVolume()
+        {
+            if (_volume < 0)
+            {
+                throw new Exception("Cannot decrease the volume less than 0.");
+            }
+            _volume -= 1;
+            return _volume;
         }
     }
     // implement a class Radio that corresponds to the Class diagram 
---
+++ RadioAppStarterCode/RadioTests/RadioVolumeTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using RadioApp;
+using System;
+
+
+namespace RadioTests
+{
+    class RadioVolumeTests
+    {
+        private Radio _radio;
+        [SetUp]
+        public void Setup()
+        {
+            _radio = new Radio();
+            _radio.TurnOn();
+        }
+
+        [TestCase(31)]
+        
+        public void IncreaseVolumeTest(int increasedVolume)
+        {
+            
+            Assert.AreEqual(increasedVolume, _radio.IncreaseVolume());
+        }
+
+        [Test]
+        public void IncreaseVolumeOverThan100Test()
+        {
+            for (int i = 1; i < 71; i++)
+            {
+                _radio.IncreaseVolume();
+            }
+
+            Assert.AreEqual(101, _radio.IncreaseVolume());
+            var ex = Assert.Throws<Exception>(() => _radio.IncreaseVolume());
+            //Assert.AreEqual("Cannot increase the volume over 100.", ex.Message);
+            //Assert.AreEqual("Can not increase volume over 100.");
+        }
+
+        [TestCase(29)]
+
+        public void DecreaseVolumeTest(int decreasedVolume)
+        {
+
+            Assert.AreEqual(decreasedVolume, _radio.DecreaseVolume());
+        }
+
+        [Test]
+        public void DecreaseVolumeLessThan0Test()
+        {
+            for (int i = 1; i < 31; i++)
+            {
+                _radio.DecreaseVolume();
+            }
+
+            Assert.AreEqual(-1, _radio.DecreaseVolume());
+            var ex = Assert.Throws<Exception>(() => _radio.DecreaseVolume());
+            //Assert.AreEqual("Cannot increase the volume over 100.", ex.Message);
+            //Assert.AreEqual("Can not increase volume over 100.");
+        }
+    }
+}
