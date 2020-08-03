863586d82c6983f44eee67c2a06429e0a0b66721
LeoRoma <xiajtn@gmail.com>
2020-08-03 11:30:03 +0100

    Cat db added

---
+++ SQL/catdb.sql
@@ -0,0 +1,20 @@
+﻿--use master;
+
+--create database CatDB
+--go
+
+--use CatDB
+--go
+
+--create table Cats(
+--	CatID int not null identity primary key,
+--	CatName nvarchar(50),
+--	Age int,
+--	Description nvarchar(50)
+--)
+
+--insert into Cats values ('Ozzy', 15, 'Mainecoon')
+--insert into Cats values ('Lucy', 15, 'Sphinx')
+--insert into Cats values ('Jasper', 15, 'Great cat')
+
+select * from Cats
