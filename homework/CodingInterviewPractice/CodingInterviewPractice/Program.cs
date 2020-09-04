09ffa5c055db23e2fd6a00daaf91ee6f0f5aee55
LeoRoma <xiajtn@gmail.com>
2020-09-04 21:59:42 +0100

    Fizzbuzz completed

--- homework/CodingInterviewPractice/CodingInterviewPractice/Program.cs
+++ homework/CodingInterviewPractice/CodingInterviewPractice/Program.cs
@@ -6,7 +6,32 @@ namespace CodingInterviewPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(FizzBuzz(i));
+            }
+           
+        }
+
+        // Fizzbuzz
+        public static string FizzBuzz(int num)
+        {
+            if(num % 15 == 0)
+            {
+                return "Fizzbuzz";
+            }
+            else if(num % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if(num % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return num.ToString();
+            }
         }
     }
 }
